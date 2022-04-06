c062fa78942c54d694b1138eafb8412e38e64a81
315 <315@315NEW-14>
2022-04-06 13:17:48 +0300

    Реализована кнопка регистрации

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,12 +27,56 @@ namespace TestLogin_Romanov
 
         private void Button_Reg_Click(object sender, RoutedEventArgs e)
         {
-            string login = textBoxLogin.Text;
-            string pass = textBoxPass.Password;
-            string pass_ = textBoxPass_.Password;
-            string email = textBoxEmail.Text;
+            string login = textBoxLogin.Text.Trim();
+            string pass = textBoxPass.Password.Trim();
+            string pass_ = textBoxPass_.Password.Trim();
+            string email = textBoxEmail.Text.ToLower().Trim();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Поле введено не верно";
+                textBoxLogin.Background = Brushes.Red;
+            } else
+
+            if (pass.Length < 6)
+            {
+                textBoxPass.ToolTip = "Пароль должен содержать не менее 6 символов";
+                textBoxPass.Background = Brushes.Red;
+            } else 
+
+            if (pass.Length != pass_.Length )
+            {
+                textBoxPass.ToolTip = "Введеные пароли не совпадают";
+                textBoxPass_.ToolTip = "Введеные пароли не совпадают";
+                textBoxPass.Background = Brushes.Red;
+                textBoxPass_.Background = Brushes.Red;
+            } else 
+            
+            if(email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Почта указана неверно";
+                textBoxEmail.Background = Brushes.Red;
+                
+            } else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                
+                textBoxPass.ToolTip = "";
+                textBoxPass.Background = Brushes.Transparent;
+                textBoxPass_.ToolTip = "";
+                textBoxPass_.Background = Brushes.Transparent;
+
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Регистрация прошла успешно!", "Результат");
+            }
 
 
+
+
+            // MessageBox.Show($"Login: {login} Pas: {pass} Email: {email}", "Введенные данные:");
         }
     }
 }
